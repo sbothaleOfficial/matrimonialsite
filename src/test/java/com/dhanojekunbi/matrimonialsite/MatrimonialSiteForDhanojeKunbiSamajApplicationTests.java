f212ee38515850e50ed5dc08ae981f68194ef34b
sbothaleOfficial <sbothale.official@gmail.com>
2023-11-28 14:01:56 +0530

    first commit

---
+++ src/test/java/com/dhanojekunbi/matrimonialsite/MatrimonialSiteForDhanojeKunbiSamajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhanojekunbi.matrimonialsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrimonialSiteForDhanojeKunbiSamajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
